0848cdf4ba576279b5ac9dbda3a255ab27335915
Resumen Electoral España <49616870+resumenelectoral@users.noreply.github.com>
2019-04-19 17:43:33 +0000

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,3 @@
 $(".box-item").click(function() {window.location = $(this).find("a").attr("href"); return false;});
+$(".btn-spe-play").click(function() {var spe_id = $(this).attr('spe_id'); $(".btn-spe-play").show(); $(".btn-spe-pause").hide(); $(this).hide(); $("#btn-pause-"+spe_id).show(); return false;});
+$(".btn-spe-pause").click(function() {var spe_id = $(this).attr('spe_id'); $(this).toggle(); $("#btn-play-"+spe_id).toggle(); return false;});
